d56daabd8caa5f8c42ffafedea32c2062d23c6fc
DurgaJha <dj.gunjan2012@gmail.com>
2021-07-11 20:43:51 +0545

    Update

--- Introduction.py
+++ Introduction.py
@@ -1,5 +1,15 @@
-x = 10
-print(type(x))
-print(dir(x))
-name = "Ram"
-print(dir(name))
+# x = 10
+# print(type(x))
+# print(dir(x))
+# name = "Ram"
+# print(dir(name))
+# y = 3.45455
+# print("%.2f" % y)
+# print(round(y,2))
+# x = "python"
+# print(x.upper())
+# print(f"{x} {y}")
+# y = input("Enter any number: ")
+# x = input("Enter any number: ")
+# print(int(x)+int(y))
+
